fc2ae62a2ebab1a854e18473d1c093f48ab9d1ce
david-kondepati <davidsvk49@gmail.com>
2025-05-01 14:23:26 +0530

    2ns commit

--- MavenFirst/src/main/java/com/nit/main/App.java
+++ MavenFirst/src/main/java/com/nit/main/App.java
@@ -6,5 +6,8 @@ package com.nit.main;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int a =10;
+        int b = 20;
+        int c = a+b;
     }
 }
