31ddfc63c33df23304337cadecc558b09afafaf6
david-kondepati <davidsvk49@gmail.com>
2025-05-08 16:59:34 +0530

    hello

--- MavenFirst/src/test/java/com/nit/main/AppTest.java
+++ MavenFirst/src/test/java/com/nit/main/AppTest.java
@@ -4,9 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import lombok.Setter;
+
 /**
  * Unit test for simple App.
  */
+@Setter
 public class AppTest {
 
     /**
